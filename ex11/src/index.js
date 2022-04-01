b43b198e2375e64bd748632bdffb1fb5569eb211
Lunjalook <Azra@admins-MacBook-Air.local>
2022-04-01 23:56:59 +0200

    Adding exercise 11 - str immutanility

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,7 +1,7 @@
 function main() {
     // only change code below this line
     var myString = "Jello World";
-    myString = "Hello World"
+    myString = "Hello World";
     // only change code above this line
     return myString;
 }
